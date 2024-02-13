8563c82c55235020d70cb2a9ccbe15081e958fa4
nmoliner <nmolimon@myuax.com>
2024-02-13 16:27:14 +0100

    cambiops

---
+++ ejercicio1.py
@@ -0,0 +1,8 @@
+#Identifica el tipo de dato (int, float, string o list) de los siguientes valores literales:
+
+# Valores literales
+valores = ["Hola Mundo", [1, 10, 100], -25, 1.167, ["Hola", "Mundo"]]
+
+# Iterar sobre los valores y mostrar su tipo
+for valor in valores:
+    print(f"El tipo de dato de {valor} es {type(valor).__name__}")
