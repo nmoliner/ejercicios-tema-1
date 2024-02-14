e2a9b9bcc1fa2a8a872cb6ca3bd092cd9f3fddd0
nmoliner <nmolimon@myuax.com>
2024-02-14 10:49:20 +0100

    Update ejercicio1.py
    hazme un codigo

--- ejercicio1.py
+++ ejercicio1.py
@@ -6,5 +6,3 @@ valores = ["Hola Mundo", [1, 10, 100], -25, 1.167, ["Hola", "Mundo"]]
 # Iterar sobre los valores y mostrar su tipo
 for valor in valores:
     print(f"El tipo de dato de {valor} es {type(valor).__name__}")
-
-Check for Updates
